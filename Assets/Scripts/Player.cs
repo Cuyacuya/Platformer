15ec4080f0844dec6527a84d22df4c7f0f43e8d1
Cuyacuya <59507274+Cuyacuya@users.noreply.github.com>
2024-09-23 00:35:53 +0900

    🚀 Feat : #3 Player 기능 추가
    - Idle, Run, Jump 기능 구현
    - 각 애니메이션 추가
    - 애니메이션 적용 스크립트 수정 필요
    
    제대로 커밋되지 않아 재 커밋

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float Speed = 5f;
-
     private Rigidbody2D rigid;
     private SpriteRenderer sprite;
     private Animator anim;
+
+    public float Speed = 5f;
+    public float JumpSpeed = 5f;
+    public float JumpCnt = 0;
+
+    private bool isGrounded;
 
 
     void Start()
@@ -20,21 +24,17 @@ public class Player : MonoBehaviour
 
     void Update()
     {
-        
-    }
-    void FixedUpdate()
-    {
-        Move();
+        OnMove();
         FlipSprite();
+        OnJump();
     }
 
-    void Move()
+    void OnMove()
     {
         float moveX = Input.GetAxisRaw("Horizontal");
-        float moveY = Input.GetAxisRaw("Vertical");
 
-        Vector2 moveInput = new Vector2(moveX, moveY).normalized; // 벡터 정규화
-        rigid.velocity = moveInput * Speed;
+        Vector2 moveInput = new Vector2(moveX, 0f).normalized; // 벡터 정규화
+        rigid.velocity = new Vector2(moveInput.x * Speed, rigid.velocity.y);
 
         anim.SetBool("isRunning", moveX != 0);
     }
@@ -47,5 +47,38 @@ public class Player : MonoBehaviour
             sprite.flipX = true;
         else if (FlipPlayer > 0)
             sprite.flipX = false;
+    }
+
+    void OnJump()
+    {
+        if(Input.GetButtonDown("Jump") && (isGrounded || JumpCnt < 2)) 
+        {
+            rigid.velocity = Vector2.zero; //1단 점프 후 2점프 시 최대 높이의 점프 가능
+            rigid.AddForce(Vector2.up * JumpSpeed, ForceMode2D.Impulse);
+            JumpCnt++;
+
+            anim.SetBool("isJumping", true);
+            anim.SetBool("isIdling", false);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+            JumpCnt = 0;
+
+            anim.SetBool("isJumping", false);
+            anim.SetBool("isIdling", true);
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
     }
 }
